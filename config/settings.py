f3743f855ce58deab1acb340d6be7bb250b6580e
sourabh <sourabh7singh@gmail.com>
2018-04-27 16:08:00 +0530

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'onlease_db',
-        'USER': 'sourabh',
+        'USER': 'postgres',
         'PASSWORD': 'Sourabh@123',
         'HOST': '',
         'PORT': ''
