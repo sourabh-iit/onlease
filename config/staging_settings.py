9d7ddce0daa915df3a90352bf5e45f3cec692344
sourabh <sourabh7singh@gmail.com>
2018-05-07 16:56:17 +0530

    navigation bar and handshake icon

--- config/staging_settings.py
+++ config/staging_settings.py
@@ -140,7 +140,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/files/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
