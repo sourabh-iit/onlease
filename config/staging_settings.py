26d7166dd843c46890f9de17bf4017b45d813a08
sourabh <sourabh7singh@gmail.com>
2018-05-07 16:47:58 +0530

    navigation bar and handshake icon

--- config/staging_settings.py
+++ config/staging_settings.py
@@ -149,8 +149,6 @@ STATICFILES_FINDERS = (
   'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
