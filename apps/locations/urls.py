3bdaf1c611556f2e8121d7c378ddfc10095f6aca
sourabh <sourabh7singh@gmail.com>
2018-08-15 15:26:51 +0530

    select import removed

--- apps/locations/urls.py
+++ apps/locations/urls.py
@@ -1,10 +1,8 @@
 from django.urls import re_path, path
 from . import views
-from django_select2.views import AutoResponseView
 
 
 app_name='locations'
 urlpatterns = [
     path('regions2/',views.regions_view,name="regions2"),
-    path('regions',AutoResponseView.as_view(),name="regions"),
 ]
