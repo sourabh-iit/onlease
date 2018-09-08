f500e1f154abc9ce9f8db4e6e2369d328f09fd84
sourabh <sourabh7singh@gmail.com>
2018-09-08 20:47:15 +0530

    credentials true

--- static/js/script.js
+++ static/js/script.js
@@ -350,6 +350,7 @@ function login_form_validation(){
         rules: {
             username: {
                 number_or_email: true,
+                required: true,
             },
             password: {
                 required: true,
@@ -1883,36 +1884,19 @@ function initialize_other_select(){
 
 $('document').ready(function(){
 
-  $.ajaxSetup({ 
-    beforeSend: function(xhr, settings) {
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie != '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = jQuery.trim(cookies[i]);
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
-                }
-            }
-            return cookieValue;
-        }
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    } 
-  });
+  $.ajaxSetup({
+    xhrFields: {
+       withCredentials: true
+    },
+    crossDomain: true
+  })
   
   $(document).ajaxSend(function (event, jqxhr, settings) {
     settings.data += '&csrfmiddlewaretoken=' + window.getCookie('csrftoken');
     if (settings.type == "POST") {
       jqxhr.setRequestHeader('X-CSRFToken', window.getCookie('csrftoken'));
-    }
-  })
+    };
+  });
 
   // $(document).ajaxComplete(function (event, xhr, settings) {
   //   debugger
