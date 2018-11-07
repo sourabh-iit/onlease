12467e265f7f5f9d6680d2ad75f9d7c876e7ca80
sourabh <sourabh7singh@gmail.com>
2018-11-07 18:37:39 +0530

    removed staticfiles_dir

--- config/common_settings.py
+++ config/common_settings.py
@@ -127,10 +127,6 @@ STATICFILES_FINDERS = (
   'sass_processor.finders.CssFinder',
 )
 
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR,'sass')
-]
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
