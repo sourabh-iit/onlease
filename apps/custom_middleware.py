4e1ea0b3de988ab293eab06b11f98bc7f005fcdf
sourabh <sourabh7singh@gmail.com>
2020-09-10 00:34:24 +0530

    logging fix

--- apps/custom_middleware.py
+++ apps/custom_middleware.py
@@ -8,8 +8,6 @@ class LogErrorMiddleware:
   def __init__(self, get_response):
     self.get_response = get_response
 
-  def __call__(self, request):
-    response = self.get_response(request)
-    # if response.status_code >= 400:
-    logger.error(response.content)
-    return response
+  def process_exception(request, exception):
+    logger.error(exception)
+    return self.get_response(request)
