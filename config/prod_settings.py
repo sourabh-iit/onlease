a63771981a9ed762bcc3553552bfd8fa60ad9685
sourabh <sourabh7singh@gmail.com>
2020-09-19 19:44:21 +0530

    settings and gitignore updated

---
+++ config/prod_settings.py
@@ -0,0 +1,51 @@
+from .common_production_settings import *
+import os
+
+ALLOWED_HOSTS = ['onlease.in','www.onlease.in','103.242.116.203', 'onlease.herokuapp.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onlease',
+        'USER': os.environ.get('DB_USERNAME'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'onlease.log',
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG'
+        }
+    }
+}
+
+BASE_URL = 'https://onlease.herokuapp.com'
--- config/settings.py
+++ config/settings.py
@@ -1,51 +1,82 @@
-from .common_production_settings import *
+from .common_settings import *
 import os
 
-ALLOWED_HOSTS = ['onlease.in','www.onlease.in','103.242.116.203', 'onlease.herokuapp.com']
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = '_q$l!3v2zo#$z+pt9hawchy2xjo)3+&y!f@w=o(2(_tz+quj&#'
+
+NG_ROK = '69d1892d'
+USE_NG_ROK = True
+BASE_URL = 'http://127.0.0.1:8000'
+if USE_NG_ROK:
+    BASE_URL='https://'+NG_ROK+'.ngrok.io'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'localhost:8000', 'localhost:8001','127.0.0.1',
+  '192.168.43.197',NG_ROK+'.ngrok.io']
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'onlease',
-        'USER': os.environ.get('DB_USERNAME'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': 'sourabh',
+        'PASSWORD': 'sourabh',
         'HOST': 'localhost',
-        'PORT': ''
+        'PORT': '5432'
     }
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'onlease.log',
-            'formatter': 'verbose'
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'testlogger': {
-            'handlers':['console'],
-            'propagate': True,
-            'level':'DEBUG'
-        }
-    }
-}
+INSTAMOJO_API_KEY = 'test_6789fe46ddf86acf17557f8d030'
+INSTAMOJO_AUTH_KEY = 'test_0d6fd27a256047d31bf26b109a8'
+INSTAMOJO_SALT = b'1d83a8d73aa84093857eb269fd825d67'
+INSTAMOJO_ENDPOINT = 'https://test.instamojo.com/api/1.1/'
 
-BASE_URL = 'https://onlease.herokuapp.com'
+RECIPIENTS = ['sourabh7singh@gmail.com']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# DEFAULT_FROM_EMAIL = 'feedback@onlease.in'
+# EMAIL_HOST = 'smtp.onlease.in'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'feedback@onlease.in'
+# EMAIL_HOST_PASSWORD = os.environ.get('BIGROCK_EMAIL_PASSWORD')
+# EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = 'feedback@onlease.in'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('GMAIL_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+
+LANGUAGE_CODE = 'hi-IN'
+
+DATE_INPUT_FORMATS = ['%d-%m-%Y','%Y-%m-%d']
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     },
+#     'select2': {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/2",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
