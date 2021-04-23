1f8b4fce391767e683155fc094f66ed7e3462ea5
sourabh <sourabh7singh@gmail.com>
2021-04-23 16:51:48 +0530

    pull from remote

--- prod-build.sh
+++ prod-build.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+git pull origin master
+
 cd angular
 npm install
 npm run-script build
 cd ..
+
 docker-compose -p onlease_prod -f docker-compose-prod.yml up --build -d
--- test-build.sh
+++ test-build.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+git pull origin master
+
 cd angular
 npm install
 npm run-script build
 cd ..
+
 docker-compose -p onlease_test -f docker-compose-test.yml up --build -d
